c4efd4651bf9a68ef516ae298e253c1cadeead06
Eliot Smith <eliotsmith19@gmail.com>
2025-01-15 11:12:06 +0000

    Cleared up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-puts "Creating recipes!"
+puts "Creating more recipes!"
 
-Recipe.create!(name: "Lasagne", description: "A delicious homebaked Italian Classic", image_url: "https://santokuknives.co.uk/cdn/shop/articles/Lasagne_300-557617.jpg?v=1724841861&width=1920", rating: 8.6)
-Recipe.create!(name: "Chicken Gyros", description: "A quick and simple", image_url: "https://www.kitchensanctuary.com/wp-content/uploads/2021/07/Chicken-Gyros-Square-FS-58.jpg", rating: 9.5 )
-Recipe.create!(name: "Pesto Pasta", description: "A simple yet flavorful dish featuring al dente pasta tossed in a vibrant basil pesto sauce.", image_url: "https://www.mrsjoneskitchen.com/wp-content/uploads/2022/02/Tuna-pesto-pasta.jpg", rating: 7.8)
-Recipe.create!(name: "Thai Red Curry", description: "A rich and aromatic dish that combines tender meat or tofu with a creamy coconut milk ba", image_url: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/thai-red-curry-34c1e6d.jpg", rating: 7.4)
+Recipe.create!(name: "Margarita Pizza", description: "A classic Italian dish known for its simplicity and fresh ingredients. It features a thin, crisp crust topped with a vibrant tomato sauce, creamy mozzarella cheese, and fresh basil leaves.", image_url: "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067", rating: 10)
+Recipe.create!(name: "Fish & Chips", description: "A beloved British dish consisting of crispy, golden-battered fish paired with thick-cut fries.", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Fish_and_chips_blackpool.jpg/1200px-Fish_and_chips_blackpool.jpg", rating: 6.8)
+# Recipe.create!(name: "", description: "", image_url: "", rating: )
 
 puts "You have added #{Recipe.count} recipes!"
